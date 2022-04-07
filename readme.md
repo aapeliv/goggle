4d801da9eebfc3fcabc100ceb29efc8ed717deeb
Aapeli <git@aapelivuorinen.com>
2022-04-07 12:53:02 -0400

    Move xml node processing into its own function and use Document class

--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
 # The Wikipedia Goggle
 
+We use [`bazel`](https://bazel.build) as the build system.
+
+## Running tests
+
+```sh
+bazel test //...
+```
+
+## Running a given executable
+
+```sh
+# bazel run //directory:target
+# e.g.
 bazel run //src:server
+# target comes from BUILD file `name` attribute
+```
--- src/extractor/extractor.cc
+++ src/extractor/extractor.cc
@@ -3,8 +3,47 @@
 #include <memory>
 
 #include "glog/logging.h"
+#include "src/doc.h"
 #include "third_party/bzip2/bzlib.h"
 #include "third_party/rapidxml/rapidxml.hpp"
+
+std::unique_ptr<Document> ParseXml(rapidxml::xml_node<>* page_node) {
+  // expect all children to just be pages
+  CHECK(std::string(page_node->name()) == "page");
+
+  uint32_t id = 0;
+  std::string title{};
+  std::string text{};
+  bool is_redirect = false;
+
+  for (auto child_node = page_node->first_node(); child_node != nullptr;
+       child_node = child_node->next_sibling()) {
+    std::string name{child_node->name()};
+    if (name == "id") {
+      id = atoi(child_node->value());
+    } else if (name == "title") {
+      title = child_node->value();
+    } else if (name == "revision") {
+      // further child node
+      text = child_node->first_node("text")->value();
+    } else if (name == "redirect") {
+      is_redirect = true;
+      break;
+    }
+  }
+
+  if (is_redirect) {
+    LOG(INFO) << "Found redirect page, skipping";
+    return nullptr;
+  } else if (id == 0 || title == "" || text == "") {
+    LOG(WARNING) << "Incomplete page: id=" << id << ", title=" << title;
+    return nullptr;
+  } else {
+    LOG(INFO) << "Found page; id=" << id << ", title=" << title
+              << ", text length=" << text.size();
+    return std::make_unique<Document>(id, title, text);
+  }
+}
 
 int main() {
   std::ifstream dump1;
@@ -32,7 +71,7 @@ int main() {
 
   // decompress the stream
   int res = BZ2_bzBuffToBuffDecompress(
-      decompressed.get(), reinterpret_cast<unsigned int *>(&dlen_out),
+      decompressed.get(), reinterpret_cast<unsigned int*>(&dlen_out),
       data.get(), len, 0, 0);
   CHECK(res != BZ_OUTBUFF_FULL) << "Didn't reserve enough memory.";
   CHECK(res == 0) << "Failed to decompress";
@@ -48,38 +87,9 @@ int main() {
   // first page
   for (auto page_node = xml.first_node(); page_node != nullptr;
        page_node = page_node->next_sibling()) {
-    LOG(INFO) << page_node->name();
-    // expect all children to just be pages
-    CHECK(std::string(page_node->name()) == "page");
-
-    uint32_t id = 0;
-    std::string title{};
-    std::string text{};
-    bool is_redirect = false;
-
-    for (auto child_node = page_node->first_node(); child_node != nullptr;
-         child_node = child_node->next_sibling()) {
-      std::string name{child_node->name()};
-      if (name == "id") {
-        id = atoi(child_node->value());
-      } else if (name == "title") {
-        title = child_node->value();
-      } else if (name == "revision") {
-        // further child node
-        text = child_node->first_node("text")->value();
-      } else if (name == "redirect") {
-        is_redirect = true;
-        break;
-      }
-    }
-
-    if (is_redirect) {
-      LOG(INFO) << "Found redirect page, skipping";
-    } else if (id == 0 || title == "" || text == "") {
-      LOG(WARNING) << "Incomplete page: id=" << id << ", title=" << title;
-    } else {
-      LOG(INFO) << "Found page; id=" << id << ", title=" << title << ", text:";
-      LOG(INFO) << text;
+    auto doc = ParseXml(page_node);
+    if (doc != nullptr) {
+      LOG(INFO) << doc->get_title();
     }
   }
 
