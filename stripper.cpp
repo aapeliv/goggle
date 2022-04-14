5dfe0c296f9ebf88c91436582127bad3307b35a2
Katie Kim <jk4534@columbia.edu>
2022-04-14 14:01:25 -0400

    text stripper complete

--- stripper.cpp
+++ stripper.cpp
@@ -7,18 +7,18 @@ using std::cout;
 // we return the flattened version of the text
 
 // title is between <title> and </title>
-// text is between <text> and </text> but we need to remove some segments before and after
+// text is between <text> and </text>
 
 string find_title(const string s); 
 string strip_text(const string s); // eventually modify in-place
 
 int main()
 {
-    // const std::string s = "<title>Anarchism</title>";
-    // cout << find_title(s) << "\n";
+    const std::string s = "<title>Anarchism</title>";
+    cout << "Title: " << find_title(s) << "\n";
 
     const string test2 = "<text bytes=\"104699\" xml:space=\"preserve\">{{short description|Political philosophy and movement}}{{other uses}} '''Anarchism''' is a [[political philosophy]] and [[Political movement|movement]] that is sceptical of [[authority]] and rejects all involuntary, coercive forms of [[Social hierarchy|hierarchy]]. Anarchism calls for the abolition of the [[State (polity)|state]],</text>";
-    cout << strip_text(test2) << "\n"; 
+    cout << "Text: " << strip_text(test2) << "\n"; 
 
     return 0; 
 
@@ -32,7 +32,7 @@ string find_title(const string s)
     if (std::regex_search(s.begin(), s.end(), match, rgx))
         return match[1]; 
     
-    return ""; // nothing is found should throw error
+    return ""; // no title found
 }
 
 string strip_text(const string s)
@@ -48,34 +48,27 @@ string strip_text(const string s)
     // else
     //     cout << "No match\n"; 
     copy = std::regex_replace(s, rgx, "$1");
-    cout << copy << "\n";
 
     // 2. remove everything from References onwards (if References exists)
-    cout << "There\n";
-    cout << copy << "\n";
     if (copy.find("== References ==")) 
         copy = copy.substr(0, copy.find("== References =="));
         
-    cout << "Here\n";
-    cout << copy << "\n";
     // 3. strip unnecessary formatting
     std::regex rgx2("[{{].*[}}]"); // if between {{ }} delete whole thing
     copy = std::regex_replace(copy, rgx2, "");  
-    cout << "copy here " << copy << "\n";
-    // ([^\]]+)
-    // ^(?=.*|)(?!.*\\]\\]).*
-    std::regex rgx3("\\[\\[^(?=.*|)(?!.*\\]\\]).*\\]\\]");
-    // std::regex rgx3("\\[\\[ .+(?!\\]\\])\\|(.+) \\]\\]");
-    // std::regex rgx3("\\[\\[(.+)\\|(.+)\\]\\]"); // between [[ ]] with a | in the middle
-    copy = std::regex_replace(copy, rgx3, "$1"); // choose second word
-    cout << "copy2 " << copy << "\n"; 
+    
+    // std::regex rgx3(R"(\[\[([^\[\]\|]+) \| ([^\[\]\|]+)\]\])");
+    std::regex case1(R"(\[\[([^\[\]\|]+)\]\])");
+    std::regex case2(R"(\[\[([^\[\]\|]+)\|([^\[\]\|]+)\]\])");
+    copy = std::regex_replace(copy, case1, "$1");
+    copy = std::regex_replace(copy, case2, "$2");
 
     // // between [[ ]] without a | in the middle -- just remove [[ and ]]
-    std::regex rgx4("\\[\\["); 
-    std::regex rgx5("\\]\\]");
+    // std::regex rgx4("\\[\\["); 
+    // std::regex rgx5("\\]\\]");
     std::regex rgx6("'''");
-    copy = std::regex_replace(copy, rgx4, ""); 
-    copy = std::regex_replace(copy, rgx5, ""); 
+    // copy = std::regex_replace(copy, rgx4, ""); 
+    // copy = std::regex_replace(copy, rgx5, ""); 
     copy = std::regex_replace(copy, rgx6, ""); 
 
     return copy;
