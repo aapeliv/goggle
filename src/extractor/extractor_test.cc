ad91f948fb748e099cc4887d395774b9c52980e7
Aapeli <git@aapelivuorinen.com>
2022-04-23 13:34:07 -0400

    Clean up stuff and start on gluing everything together

--- src/extractor/extractor_test.cc
+++ src/extractor/extractor_test.cc
@@ -10,9 +10,9 @@
 TEST(Extractor, Basic) {
   size_t title_len = 0;
   extract_dump(
-      "src/extractor/"
+      "data/"
       "enwiki-20220401-pages-articles-multistream-index1.txt-p1p41242.bz2",
-      "src/extractor/"
+      "data/"
       "enwiki-20220401-pages-articles-multistream1.xml-p1p41242.bz2",
       [&](std::unique_ptr<Document> doc) {
         title_len += doc->get_title().size();
--- src/server.cc
+++ src/server.cc
@@ -1,15 +1,53 @@
-#include <iostream>
+#include <memory>
 
 #include "glog/logging.h"
 #include "httplib.h"
+#include "src/doc.h"
+#include "src/extractor/extractor.h"
+#include "src/forward_index/forward_index.h"
+#include "src/trigram/trigram.h"
 
 int main() {
-  LOG(INFO) << "Starting Wikipedia Goggle.";
+  LOG(INFO) << "Starting The Wikipedia Goggle.";
+
+  DocIndex forward_ix{};
+  TrigramIndex tri_ix{};
+
+  extract_dump(
+      "data/"
+      "enwiki-20220401-pages-articles-multistream-index1.txt-p1p41242.bz2",
+      "data/"
+      "enwiki-20220401-pages-articles-multistream1.xml-p1p41242.bz2",
+      [&](std::unique_ptr<Document> doc) {
+        tri_ix.AddDocument(doc->get_id(), doc->get_text());
+        forward_ix.AddDocument(
+            Document(doc->get_id(), doc->get_title(), doc->get_text()));
+      });
 
   httplib::Server srv;
-  srv.Get("/", [](const httplib::Request &, httplib::Response &res) {
+  srv.Get("/", [](const httplib::Request&, httplib::Response& res) {
     res.set_content("{\"msg\": \"hello world\"}",
                     "application/json; charset=utf-8");
+  });
+
+  srv.Get("/query", [&](const httplib::Request& req, httplib::Response& res) {
+    if (!req.has_param("q")) {
+      res.set_content("{\"error\": \"missing query\"}",
+                      "application/json; charset=utf-8");
+    }
+    auto query = req.get_param_value("q");
+    std::stringstream ss{};
+    ss << "{\"results\": [";
+    for (auto&& doc_id : tri_ix.FindPossibleDocuments(query)) {
+      auto doc = forward_ix.GetDocument(doc_id);
+      ss << "{";
+      ss << "\"id\": " << doc.get_id() << ",";
+      ss << "\"title\": \"" << doc.get_title() << "\",";
+      // ss << \"text\": " << doc.get_text();
+      ss << "},";
+    }
+    ss << "]}";
+    res.set_content(ss.str(), "application/json; charset=utf-8");
   });
 
   LOG(INFO) << "Serving on 8080.";
