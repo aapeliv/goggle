9075ec51f3587fc13e4360955b440525bb6321e3
Aapeli <git@aapelivuorinen.com>
2022-04-29 22:00:59 -0400

    Make frontend pretty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,15 @@
-import './App.css';
-import DATA from './MOCK_DATA.json'
-import {useState} from 'react'
+import "./App.css";
+import { useState } from "react"
 import axios from "axios"
 
 const API_BASE = "http://localhost:8080"
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("")
   const [response, setResponse] = useState(null)
 
   const fetchResults = (keyword) => {
     axios.get(API_BASE + "/query", {
-      params: {
-        "q": keyword,
-        // page length
-        "pl": 10,
-      }
+      params: { "q": keyword }
     }).then((res) => {
       console.log(res)
       setResponse(res.data)
@@ -24,52 +18,33 @@ function App() {
 
   return (
     <div className="App">
-      <input 
-      type = "text" 
-      placeholder = "Search String" 
-      // onChange = {(event) => {
-      //   setSearchTerm(event.target.value);
-      // }}
-      onKeyDown={(event) => {
-        if (event.key=== 'Enter'){
-          console.log("Enter key pressed")
-          // setSearchTerm(event.target.value)
-          fetchResults(event.target.value)
-        }
-      }}
+      <div className="tip">Source code on <a href="https://github.com/aapeliv/goggle">GitHub</a>.</div>
+      <img src="/goggle_text.png" height="200" /><br />
+      <input
+        type="text"
+        placeholder="Search 6,487,373 Wikipedia articles"
+        className="box"
+        onKeyDown={(event) => {
+          if (event.key === "Enter"){
+            console.log("Enter key pressed")
+            fetchResults(event.target.value)
+          }
+        }}
       /><br />
+      <div className="result-box">
       {response ? (
         <>
-          <small>Returned results in {response["duration_ms"]} ms</small>
+          <div className="hint">Results returned in {response["duration_ms"]/1000} s.</div>
           {response["results"].map(res => (
-            <>
-              <br />
-              <b>{res["title"]}</b><br />
-              <p>Rank: {res["pagerank"]}, id: {res["id"]}</p>
-              <hr/>
-            </>))}
-        </>
-      )
-        :
-      (<div>No results yet</div>)
+            <div className="result">
+              <p className="page-title"><a href={res["url"]}>{res["title"]}</a></p>
+              <p className="page-snippet">...<span dangerouslySetInnerHTML={{__html: res["snippet"]}} />... <a href={res["url"]}>Read more...</a></p>
+            </div>))}
+        </>)
+          :
+        (<div className="hint">Type something and press enter to search.</div>)
       }
-        <br />
-        <br />
-        <br />
-        <br />
-        <b>DEBUG:</b><br />
-        <code>{JSON.stringify(response)}</code>
-      {/* {DATA.filter((val) => {
-        if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
-          return val;
-        }
-      }).map((val, key) => {
-        return (
-          <div classname = "document" key = {key}>
-            <p>{val.first_name}</p>
-          </div>
-        );
-      })} */}
+      </div>
     </div>
   );
 }
